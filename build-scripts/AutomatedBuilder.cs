dc1493c10236597fe840e6ec6a6412be5bf697e3
Taylor Park <parktwut@gmail.com>
2019-01-04 22:15:41 -0700

    Added Build Scripts

---
+++ build-scripts/AutomatedBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AutomatedBuild
+{
+	public static void Build()
+	{
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = new[] { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/Level I.unity", "Assets/Scenes/Level II.unity", "Assets/Scenes/Level III.unity" };
+		buildPlayerOptions.locationPathName = "/data/builds/PROJECT_NAME/";
+		buildPlayerOptions.target = BuildTarget.WebGL;
+		buildPlayerOptions.options = BuildOptions.None;
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+		#buildPlayerOptions.locationPathName = "/data/builds/PROJECT_NAME.apk";
+		#buildPlayerOptions.target = BuildTarget.Android;
+		#BuildPipeline.BuildPlayer(buildPlayerOptions);
+	}
+}
